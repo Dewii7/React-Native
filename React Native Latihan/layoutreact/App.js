d62bf1a9110b6530b3b9880084e4b5ca5a65cf00
goddess.guy <goddess.guy@gmail.com>
2021-06-02 16:48:33 +0800

    layout react native

--- React Native Latihan/layoutreact/App.js
+++ React Native Latihan/layoutreact/App.js
@@ -17,7 +17,7 @@ const Stack = createStackNavigator();
 
 const App = () => {
   const [active, setActive] = useState(0);
-  const tabData = ['Home', 'About', 'Products', 'Contact'];
+  const tabData = ['Home', 'About', 'Products', 'Contact', 'Profile'];
   const moveData = index => {
     setActive(index);
   };
